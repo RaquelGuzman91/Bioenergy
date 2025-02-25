3b7dc76261cdd2ec4f29dcc0a2ae06b71d226235
RaquelGuzman91 <rakelguzman91@gmail.com>
2025-02-25 00:38:48 -0600

    cambios a elemento nav a paginas

--- main.js
+++ main.js
@@ -117,3 +117,12 @@ let CounterObserver = new IntersectionObserver(
 CounterObserver.observe(section_counter);
 
 
+ /*===================   efecto mostrar y esconder nav al hacer scrol    ====================*/
+ var zero = 0;
+ $(document).ready(function(){
+     $(window).on('scroll', function(){
+         $('.navbar').toggleClass('hide', $(window).scrollTop()
+         > zero);
+         zero = $(window).scrollTop();
+     })
+})
