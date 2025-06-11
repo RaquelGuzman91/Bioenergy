4cec2bdb10149efa0b48475e9c86efe7bc5aa07b
RaquelGuzman91 <raquelguzman91@gmail.com>
2025-06-11 08:40:37 -0600

    nuevos cambios a banner y button

--- btn-boton.js
+++ btn-boton.js
@@ -9,19 +9,23 @@ addEventListener('DOMContentLoaded', () =>{
     const irArriba = () => {
         if (obtener_pixeles_inicio() > 0) {
             requestAnimationFrame(irArriba)
-            scrollTo(0, obtener_pixeles_inicio() -(obtener_pixeles_inicio() / 40))
+            scrollTo(0, Math.max(0, obtener_pixeles_inicio() - (obtener_pixeles_inicio() / 10)));
         }
     }
 
     const indicarScroll = () => {
-        if(obtener_pixeles_inicio() > 300){
+        if(obtener_pixeles_inicio() > 700){
             boton_ir_arriba.className = 'mostrar'
         }   else {
             boton_ir_arriba.className = 'ocultar'
         }    
     }
 
-    boton_ir_arriba.addEventListener('click', irArriba)
-    window.addEventListener('scroll', indicarScroll)
+    boton_ir_arriba.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
-})
+    window.addEventListener('scroll', indicarScroll);
+
+});
+
